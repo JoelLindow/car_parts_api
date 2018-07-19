4c756cec27c92705090c3eb2604c96af4754ec6f
JoelLindow <joellindow@gmail.com>
2018-07-18 19:31:41 -0600

    begin parts spec creation and testing

---
+++ spec/request/parts_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'parts api' do
+
+  it 'returns a part record' do
+    create_list(:part, 5)
+
+    get "/api/v1/parts"
+
+    expect(response_to be_success) #200 response
+
+    parts = JSON.parse(response.body)
+
+    expect(parts.count).to eq(5)
+  end
+
+end
