b3c45f945365c27a2fb58f82d6de13365e0739a1
JoelLindow <joellindow@gmail.com>
2018-07-18 19:47:10 -0600

    Part factory created with name, description and OEM boolean

---
+++ spec/factories/parts.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :part do
+    sequence :name do |x|
+      "Alternator #{x}"
+    end
+
+    description "This is a nice alternator"
+    oem [true, false].sample
+  end
+
+end
