ab6d8f616318372b6c52cce84f5705912a72bda9
JoelLindow <joellindow@gmail.com>
2018-09-05 21:04:41 -0600

    hey! that post seems to be workin'

--- app/controllers/api/v1/parts_controller.rb
+++ app/controllers/api/v1/parts_controller.rb
@@ -1,36 +1,40 @@
 module Api
   module V1
-      class PartsController < ApplicationController
+    class PartsController < ApplicationController
 
-        def index
-          render json: Part.all
-        end
-
-        def show
-          render json: Part.find(params[:id])
-        end
-
-        def create
-          Part.create(strong_params)
-          # binding.pry
-        end
-
-        # CUSTOM PARTS METHODS
-        def find_show
-          # Will potentially present more than 1 item
-          render json: Part.find_by(find_params)
-        end
-
-        private
-
-        def find_params
-          params.permit(:id, :name)
-        end
-
-        def strong_params
-          params.require(:part).permit(:name, :description, :oem, :price)
-        end
-
+      def index
+        render json: Part.all
       end
+
+      def show
+        render json: Part.find(params[:id])
+      end
+
+      def create
+        x = Part.new(strong_params)
+        if x.save
+          render status: 204
+        else
+          render status: 400, json: {message: x.errors}
+        end
+      end
+
+      # CUSTOM PARTS METHODS
+      def find_show
+        # Will potentially present more than 1 item
+        render json: Part.find_by(find_params)
+      end
+
+      private
+
+      def find_params
+        params.permit(:id, :name)
+      end
+
+      def strong_params
+        params.require(:part).permit(:name, :description, :oem, :price)
+      end
+
+    end
   end
 end
